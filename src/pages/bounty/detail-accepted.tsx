d7936428527ba8264f70fe314826424dc4b35cb1
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2021-10-27 16:32:43 -0400

    Created skeleton for all pages

---
+++ src/pages/bounty/detail-accepted.tsx
@@ -0,0 +1,13 @@
+import { Box, Center, Heading, Link } from "@chakra-ui/react";
+
+const DetailAccepted = () => {
+  return (
+      <Box >
+        <Heading as="h2" size="md">
+          <Link href=".">Bounty Accepted</Link>
+        </Heading>
+      </Box> 
+  );
+};
+
+export default DetailAccepted;
